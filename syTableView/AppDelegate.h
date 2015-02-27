84a7068f83533b67970c707c7e28a57014305ab8
世缘 <1213423761@qq.com>
2015-02-27 15:26:16 +0800

    添加podspec

--- syTableView/AppDelegate.h
+++ syTableView/AppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "DemoViewController.h"
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
--- syTableView/AppDelegate.m
+++ syTableView/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "DemoViewController.h"
+
 
 @interface AppDelegate ()
 
